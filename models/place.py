59e4852eeed035b339ea5d4a1cb9f5c8ad4aa883
alaksatti <ala.k.satti@gmail.com>
2019-12-18 22:52:22 +0000

    change var names

--- models/place.py
+++ models/place.py
@@ -37,39 +37,33 @@ class Place(BaseModel, Base):
     """
 
     __tablename__ = 'places'
-    name = Column(String(128),
-                  nullable=False)
-
-    user_id = Column(String(60),
-                     ForeignKey('users.id'),
-                     nullable=False)
-
-    description = Column(String(1024),
-                         nullable=True)
 
     city_id = Column(String(60),
                      ForeignKey('cities.id'),
                      nullable=False)
-
+    user_id = Column(String(60),
+                     ForeignKey('users.id'),
+                     nullable=False)
+    name = Column(String(128),
+                  nullable=False)
+    description = Column(String(1024),
+                         nullable=True)
     number_rooms = Column(Integer,
                           nullable=False,
                           default=0)
-
     number_bathrooms = Column(Integer,
                               nullable=False,
                               default=0)
-    price_by_night = Column(Integer,
-                            nullable=False,
-                            default=0)
     max_guest = Column(Integer,
                        nullable=False,
                        default=0)
+    price_by_night = Column(Integer,
+                            nullable=False,
+                            default=0)
     latitude = Column(Float,
                       nullable=True)
-
     longitude = Column(Float,
                        nullable=True)
-
 
     amenity_ids =[]
 
@@ -96,8 +90,8 @@ class Place(BaseModel, Base):
         def amenities(self):
             ''' returns list of amenity instances based on attr amenity_ids '''
             list_amenity = []
-            amenities = models.storage.all(Amenity)
-            for amenity in amenities:
+            final = models.storage.all(Amenity)
+            for amenity in final.values():
                 if amenity.id in self.amenity_ids:
                     list_amenity.append(amenity)
             return list_amenity
