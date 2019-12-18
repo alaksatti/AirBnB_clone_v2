4a9b5e2e593c5c8123645bba3f0fdfa1d6e342ca
alaksatti <ala.k.satti@gmail.com>
2019-12-18 00:01:01 +0000

    changes to basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 import models
 from datetime import datetime
 
-
+Base = declarative_base()
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
@@ -20,17 +20,16 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+        id = Column(String(60),
+                    primary_key=True,
+                    nullable=False)
 
+        created_at = Column(DateTime, 
+                            nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(Datetime,
+                            nullable=False,
+                            default=datetime.utcnow())
     def __str__(self):
         """returns a string
         Return:
