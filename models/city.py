dfd70144f7b451cdf241572ff1d41fee1254de6c
alaksatti <ala.k.satti@gmail.com>
2019-12-18 12:03:43 +0000

    update city

--- models/city.py
+++ models/city.py
@@ -2,16 +2,18 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
     """This is the class for City
     Attributes:
+        __tablename__: the tablename
         state_id: The state id
         name: input name
     """
-
-    __tablename__ = "cities"
+    __tablename__ = 'cities'
 
     name = Column(String(128),
                   nullable=False)
@@ -20,4 +22,4 @@ class City(BaseModel, Base):
                       ForeignKey('states.id'),
                       nullable=False)
 
-
+    places = relationship("Place", backref="cities", cascade="delete")
