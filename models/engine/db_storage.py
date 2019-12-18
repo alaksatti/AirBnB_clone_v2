3e550b81c325444172c34a79d175a2b924ca9523
alaksatti <ala.k.satti@gmail.com>
2019-12-18 12:27:07 +0000

    refactor

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python3                                                                                                
+#!/usr/bin/python3
 """SQL DB Class"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import scoped_session
 from models.user import User
 from models.review import Review
 from models.amenity import Amenity
 from models.state import State
 from models.place import Place
 from models.city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-
 
 
 class DBStorage:
@@ -18,7 +20,6 @@ class DBStorage:
 
     __engine = None
     __session = None
-
 
     def __init__(self):
         """ initialize DB engine """
@@ -33,75 +34,48 @@ class DBStorage:
                                              host,
                                              db,
                                              pool_pre_ping=True))
+
         if getenv('HBNB_MYSQL_ENV') == 'test':
             Base.metadata.drop_all(bind=self.__engine)
-            
 
     def all(self, cls=None):
         """query current database session"""
-
         queried_dict = {}
         if cls:
             queried_results = self.__session.query(cls).all()
+
         else:
             queried_results = []
-            classes = ["City", "State", "Place", "User", "Amenity", "Review"]
-            for _class in classes:
-                objs = self.__session.query(_class)
-                for obj in objs:
-                    queried_results.append(obj)
-                    
+            queried_results += self.__session.query(State).all()
+            queried_results += self.__session.query(City).all()
+            queried_results += self.__session.query(User).all()
+            queried_results += self.__session.query(Place).all()
+
         for objects in queried_results:
             key = type(objects).__name__ + "." + str(objects.id)
             queried_dict[key] = objects
-            
+
         return queried_dict
 
-
     def new(self, obj):
-        """ adds object to current database session"""
-        self.__session.add(obj)
-
+        """Adds object to current database session"""
+        return self.__session.add(obj)
 
     def save(self):
-        """ commit changes to current database session"""
-        self.__session.commit()
-
+        """Commits all changes the current database session"""
+        return self.__session.commit()
 
     def delete(self, obj=None):
-        """ deletes from the current database session"""
+        """Delete obj from current DB session"""
         if obj:
-            self.__session.delete(obj)
-
+            return self.__session.delete(obj)
 
     def reload(self):
-        """creates all tables in the database"""
+        """Create all tables in the DB"""
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def close(self):
+        """Close the current session"""
+        return self.__session.close()
