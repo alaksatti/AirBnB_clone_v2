747d0eb32b8da3a4c767c4e991fabdfe73f596a7
alaksatti <ala.k.satti@gmail.com>
2019-12-18 12:12:30 +0000

    initial commit of user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -13,10 +12,23 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    places = relationship("Place", cascade="all")
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    email = Column(String(128),
+                   nullable=False)
+    password = Column(String(128),
+                      nullable=False)
+    first_name = Column(String(128),
+                        nullable=True)
+    last_name = Column(String(128),
+                       nullable=True)
+
+"""
+
+    places = relationship("Place",
+                          backref="all",
+                          cascade="delete")
+    reviews = relationship("Review",
+                           backref="user", 
+                           cascade="all")
+"""
