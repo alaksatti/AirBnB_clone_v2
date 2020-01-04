529508a9322abdd271244c5b3af51b2d42d10a21
alaksatti <ala.k.satti@gmail.com>
2020-01-04 02:05:06 +0000

    initial commit of setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get -y install nginx
+
+ufw allow 'Nginx HTTP'
+mkdir -p  /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+echo "<html>
+    <head>
+        <title>
+        Holberton School
+        </title>
+    </head>
+
+    <body>
+    </body>
+</html>" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
