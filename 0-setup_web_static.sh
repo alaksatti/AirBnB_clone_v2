4ea7a8947a866fce12a0f19bf8fb92844ae65f94
alaksatti <ala.k.satti@gmail.com>
2020-01-04 04:08:05 +0000

    plz work

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash                                                                                
 sudo apt-get update
 sudo apt-get -y install nginx
 
@@ -10,17 +10,15 @@ mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 
 echo "<html>
-    <head>
-        <title>
-        Holberton School
-        </title>
-    </head>
-
-    <body>
-    </body>
+     <head>
+	<title>
+	Holberton School
+	</title>
+     </head>
+     <body>
+     </body>
 </html>" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
