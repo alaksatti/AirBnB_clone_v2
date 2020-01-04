e3fe74c358c726b55651e3c7accdfa0f482e45a6
alaksatti <ala.k.satti@gmail.com>
2020-01-04 05:56:03 +0000

    fix

---
+++ 100-clean_web_static.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# Fabric script that distributes archive to remote server
+from fabric.api import local, env, put, run
+from datetime import datetime
+from os.path import exists, isfile
+env.hosts = ['35.196.198.246', '34.73.64.187']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/holberton'
+
+
+def do_pack():
+    """creates a .tgz archive from web_static contents"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = 'versions/web_static_' + time + '.tgz'
+    local('mkdir -p versions')
+    local('tar cvfz ' + path + ' web_static')
+    if exists(path):
+        return path
+    return None
+
+
+def do_deploy(archive_path):
+    'deploy archive to web servers'
+    if not exists(archive_path) and not isfile(archive_path):
+        return False
+    try:
+        put(archive_path, '/tmp')
+        name = archive_path.split('/')[1][:-4]
+        run('sudo mkdir -p /data/web_static/releases/' + name + '/')
+        run('sudo chown -R ubuntu:ubuntu /data')
+        run('tar -xzf /tmp/' + name + '.tgz'
+            ' -C /data/web_static/releases/' + name + '/')
+        run('rm /tmp/' + name + '.tgz')
+        run('mv /data/web_static/releases/' + name + '/web_static/* ' +
+            '/data/web_static/releases/' + name + '/')
+        run('rm -rf /data/web_static/releases/' + name + '/web_static')
+        run('rm -rf /data/web_static/current')
+        run('ln -sf /data/web_static/releases/' + name + '/ ' +
+            '/data/web_static/current')
+        print("New version deployed!")
+    except:
+        return False
+    return True
+
+
+def deploy():
+    '''Runs do_pack and do_deploy'''
+    path = do_pack()
+    if path:
+        exit_status = do_deploy(path)
+        return exit_status
+    else:
+        return False
+
+
+def do_clean(number=0):
+    '''deletes out of date archives'''
+    number = int(number)
+    if number == 0:
+        number = 1
+    keepers = local('ls -1t versions', capture=True).split('\n')[:number]
+    clean_local(keepers)
+    keep = [files[:-4] for files in keepers]
+    pass_files = run('ls -1 /data/web_static/releases').split('\n')
+    complete_files = [files.strip('\r') for files in pass_files]
+    for files in complete_files:
+        if files not in keepers:
+            run('rm -rf /data/web_static/releases/{}'.format(f))
