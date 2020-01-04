a9f0c866484b2a11cd223aed7563e69fba279cd3
alaksatti <ala.k.satti@gmail.com>
2020-01-04 05:41:40 +0000

    add print

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,12 +3,10 @@
 from fabric.api import local, env, put, run
 from datetime import datetime
 from os.path import exists, isfile
-from os import path
-
-
 env.hosts = ['35.196.198.246', '34.73.64.187']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/holberton'
+
 
 def do_pack():
     """creates a .tgz archive from web_static contents"""
@@ -25,9 +23,9 @@ def do_deploy(archive_path):
     'deploy archive to web servers'
     if not exists(archive_path) and not isfile(archive_path):
         return False
-    name = archive_path.split('/')[1][:-4]
     try:
         put(archive_path, '/tmp')
+        name = archive_path.split('/')[1][:-4]
         run('sudo mkdir -p /data/web_static/releases/' + name + '/')
         run('sudo chown -R ubuntu:ubuntu /data')
         run('tar -xzf /tmp/' + name + '.tgz'
@@ -39,8 +37,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -sf /data/web_static/releases/' + name + '/ ' +
             '/data/web_static/current')
+        print("New version deployed!")
     except:
         return False
-    
     return True
-
