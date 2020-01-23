9095ee942e9703e8863cc06812bf67dbac13d3c9
alaksatti <ala.k.satti@gmail.com>
2020-01-23 02:29:35 +0000

    cities

--- models/state.py
+++ models/state.py
@@ -29,7 +29,7 @@ class State(BaseModel, Base):
             cities = models.storage.all(City)
             list_cities = []
 
-            for city in cities:
+            for city in cities.values():
                 if city.state_id == self.id:
                     list_cities.append(city)
             return list_cities
