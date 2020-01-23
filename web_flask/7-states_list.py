2f994e1dfc0b2d8115e4802731321a7aa4c53461
alaksatti <ala.k.satti@gmail.com>
2020-01-23 02:14:38 +0000

    refactor

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 '''starts a Flask web application'''
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def display_states():
-    '''list all the states'''
-    stroage.reload()
-    dict_states = storage.all("State")
-    states = []
-    for value in dict_states.values():
-        states.append([value.id, value.name])
-    return render_template('7-states_list.html', states=states)
-
+    states = storage.all(State).values()
+    return render_template('7-states_list.html',
+                           states=states)
 
 @app.teardown_appcontext
 def teardown(exception):
-    '''tear down db'''
     storage.close()
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
