95bd74f7bc59c4280a3731d189e66721a4baaaac
alaksatti <ala.k.satti@gmail.com>
2020-01-23 08:08:38 +0000

    add module

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
